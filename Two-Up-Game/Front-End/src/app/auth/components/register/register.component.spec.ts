4d1a8ace02e29f8cb2a687ac9202727ee1a53dd8
Douglas-Thornton <douglas.thornton@chamonix.com.au>
2023-03-27 13:59:30 +1030

    Register login test created.

--- Two-Up-Game/Front-End/src/app/auth/components/register/register.component.spec.ts
+++ Two-Up-Game/Front-End/src/app/auth/components/register/register.component.spec.ts
@@ -1,25 +1,79 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { RegisterComponent } from './register.component';
-import {FormsModule} from '@angular/forms'
+import { AuthService } from './../../../services/auth.service'
+import {FormsModule, NgForm} from '@angular/forms'
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let httpMock: HttpTestingController;
+  let _auth: AuthService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule, FormsModule],
-      declarations: [ RegisterComponent ]
+      imports: [HttpClientTestingModule, RouterTestingModule,FormsModule],
+      declarations: [ RegisterComponent ],
+      providers: [AuthService]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
+
+    httpMock = TestBed.inject(HttpTestingController)
     fixture.detectChanges();
   });
+
+   afterEach(() => {
+    httpMock.verify();
+  })
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should log in a user that has registered', async () =>
+  {
+    // Set up mock response data.
+    const mockResponse = {
+      status: true,
+      data:
+      [
+        {
+          username: 'User1',
+          favColorHex: '#000000',
+          score: 100
+        }
+      ]
+    }
+
+    // Set up mock form
+    const testForm = <NgForm>
+    {
+      value:
+      {
+        password: "randomUser",
+        username: "randomUser",
+        email: "randomUser@randomUser.com.au"
+      }
+    }
+
+    component.onSubmit(testForm);
+
+    // Set up mock request
+    const mockRequest = httpMock.expectOne('http://localhost:4000/user/register');
+
+    // Assert that the correct HTTP method is used
+    expect(mockRequest.request.method).toEqual('POST');
+
+    // Respond with mock data
+    mockRequest.flush(mockResponse);
+
+    // Assert that the component has logged in a user.
+    expect(component.playerIsLoggedin).toEqual(true);
+
+  })
 });
+
