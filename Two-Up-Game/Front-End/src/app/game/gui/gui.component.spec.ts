3160276f39d96127b1d592de600989193c475352
Douglas-Thornton <douglas.thornton@chamonix.com.au>
2023-04-03 16:16:22 +0930

    GUI tests

--- Two-Up-Game/Front-End/src/app/game/gui/gui.component.spec.ts
+++ Two-Up-Game/Front-End/src/app/game/gui/gui.component.spec.ts
@@ -1,40 +1,38 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
-import { Bet, GuiComponent } from './gui.component';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { GuiComponent, Bet } from './gui.component';
 import { ApiService } from './../../services/api.service';
 import { AuthService } from './../../services/auth.service';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { user } from 'src/app/shared/user';
+import { Observable, of } from 'rxjs';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+import { CoinSpinComponent } from '../coin-spin/coin-spin.component';
 
 describe('GuiComponent', () => {
   let component: GuiComponent;
   let fixture: ComponentFixture<GuiComponent>;
-  let apiServiceSpy: jasmine.SpyObj<ApiService>;
-  let authServiceSpy: jasmine.SpyObj<AuthService>;
-  let routerSpy: jasmine.SpyObj<Router>;
+  let authService: AuthService;
+  let apiService: ApiService;
+  let httpClientModule: HttpClientModule;
 
   beforeEach(async () => {
-    let apiSpy = jasmine.createSpyObj('ApiService', ['postTypeRequest']);
-    let authSpy = jasmine.createSpyObj('AuthService', ['getPlayerDetails', 'setLoggedIn', 'logoutPlayer', 'getLoggedIn', 'setDataInLocalStorage']);
-    let routerSpy = jasmine.createSpyObj('Router', ['navigate']);
-
-    TestBed.configureTestingModule({
-      imports: [ FormsModule ],
-      declarations: [ GuiComponent ],
-      providers: [
-        { provide: ApiService, useValue: apiSpy },
-        { provide: AuthService, useValue: authSpy },
-        { provide: Router, useValue: routerSpy }
-      ]
+    await TestBed.configureTestingModule({
+      imports: [RouterTestingModule, FormsModule, HttpClientModule],
+      declarations: [ GuiComponent, CoinSpinComponent ],
+      providers: [ApiService, AuthService]
     })
     .compileComponents();
+  });
 
-    apiServiceSpy = TestBed.inject(ApiService) as jasmine.SpyObj<ApiService>;
-    authServiceSpy = TestBed.inject(AuthService) as jasmine.SpyObj<AuthService>;
-    routerSpy = TestBed.inject(Router) as jasmine.SpyObj<Router>;
-
+  beforeEach(() => {
     fixture = TestBed.createComponent(GuiComponent);
     component = fixture.componentInstance;
+    authService = TestBed.inject(AuthService);
+    apiService = TestBed.inject(ApiService);
+    httpClientModule = TestBed.inject(HttpClientModule);
     fixture.detectChanges();
   });
 
@@ -42,46 +40,76 @@ describe('GuiComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should reroll the coins on spacebar press', () => {
-    const spaceEvent = new KeyboardEvent('keydown', { key: ' ' });
-    spyOn(component, 'reroll');
-
-    window.dispatchEvent(spaceEvent);
-
-    expect(component.reroll).toHaveBeenCalled();
-  });
-
-
-  it('should navigate to login page and logout player when user is not logged in', () => {
-    authServiceSpy.getPlayerDetails.and.returnValue(null);
-
-    component.isUserLoggedIn();
-
-    expect(authServiceSpy.logoutPlayer).toHaveBeenCalled();
-    expect(routerSpy.navigate).toHaveBeenCalledWith(['login']);
-  });
-
-  it('should update PSessionScore when chosen bet matches result', () => {
+  it('should calculate result correctly for two heads bet', () => {
     component.coin_1 = true;
     component.coin_2 = true;
     component.PChosenBet = Bet.Two_Heads;
-
     component.calculateResult();
-
-    expect(component.PSessionScore).toBe(1);
+    expect(component.Result).toEqual(Bet.Two_Heads);
+    expect(component.PSessionScore).toEqual(1);
   });
 
-  it('should update PUser score and save data on submitScore', () => {
+  it('should calculate result correctly for two tails bet', () => {
+    component.coin_1 = false;
+    component.coin_2 = false;
+    component.PChosenBet = Bet.Two_Tails;
+    component.calculateResult();
+    expect(component.Result).toEqual(Bet.Two_Tails);
+    expect(component.PSessionScore).toEqual(1);
+  });
 
+  it('should calculate result correctly for even bet', () => {
+    component.coin_1 = true;
+    component.coin_2 = false;
+    component.PChosenBet = Bet.Even;
+    component.calculateResult();
+    expect(component.Result).toEqual(Bet.Even);
+    expect(component.PSessionScore).toEqual(1);
+  });
+
+  it('should add session score to user score and reset session score', () => {
     component.PUser = new user();
+    component.PUser.username = 'testuser';
+    component.PUser.score = 10;
     component.PSessionScore = 5;
-    //apiServiceSpy.postTypeRequest.and.returnValue({ status: true, data: component.PUser, token: 'testtoken' });
+
+    spyOn(apiService, 'postTypeRequest').and.returnValue(of({ status: true, data: component.PUser, token: 'testtoken' }));
+    spyOn(authService, 'setDataInLocalStorage');
 
     component.submitScore();
 
-    expect(apiServiceSpy.postTypeRequest).toHaveBeenCalledWith('user/gui', component.PUser);
-    expect(component.PUser.score).toBe(5);
-    expect(authServiceSpy.setDataInLocalStorage).toHaveBeenCalledWith('playerData', JSON.stringify(component.PUser));
-    expect(authServiceSpy.setDataInLocalStorage).toHaveBeenCalledWith('playerToken', 'testtoken');
+    expect(apiService.postTypeRequest).toHaveBeenCalledWith('user/gui', component.PUser);
+    expect(component.PUser.score).toEqual(15);
+    expect(authService.setDataInLocalStorage).toHaveBeenCalledWith('playerData', JSON.stringify(component.PUser));
+    expect(authService.setDataInLocalStorage).toHaveBeenCalledWith('playerToken', 'testtoken');
+    expect(component.PSessionScore).toEqual(0);
   });
+
+  it('should select bet type correctly', () => {
+    component.PChosenBet = Bet.Two_Heads;
+    component.PBetSelected('Two Tails');
+    expect(component.PChosenBet).toEqual(Bet.Two_Tails);
+  });
+
+
+  it('should change the users favorite colour when html binding is triggered', async () => {
+    component.PUser = new user();
+    component.PUser.username = 'testuser';
+    component.playerIsLoggedin = true;
+
+    let testValue = "#ff0000";
+    fixture.detectChanges();
+
+    console.log("CHECKING UI")
+    let colourPickerDe: DebugElement = fixture.debugElement.query(By.css('.PlayerBGColour'));
+    console.log("CHECKING DebugElement:" + colourPickerDe);
+
+    let colourPickerEl: HTMLInputElement = colourPickerDe.nativeElement;
+    console.log("CHECKING input element:" + colourPickerEl);
+
+    colourPickerEl.value = testValue;
+    colourPickerEl.dispatchEvent(new Event('input'));
+
+    expect(component.PUser.favColourHex).toEqual("#ff0000");
+  })
 });
