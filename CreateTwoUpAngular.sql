78ac87a98112242fda16eded91f34693d1f0715a
Douglas-Thornton <douglas.thornton@chamonix.com.au>
2023-04-04 14:56:10 +0930

    Added database creation script

---
+++ CreateTwoUpAngular.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE `two-up-game` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `two-up-game`;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `score` int NOT NULL DEFAULT '0',
+  `favColourHex` char(7) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
