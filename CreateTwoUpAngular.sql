ad259383c6e6c8de6a63127f128a4d517e72eaf6
Douglas-Thornton <douglas.thornton@chamonix.com.au>
2023-04-04 15:20:57 +0930

    Updating comment on sql command.
    Testing github desktop

--- CreateTwoUpAngular.sql
+++ CreateTwoUpAngular.sql
@@ -1,3 +1,5 @@
+-- This script must be run before the application will function.
+-- Must have a mysql server running on your PC.
 CREATE DATABASE `two-up-game` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `two-up-game`;
 CREATE TABLE `users` (
